8eafe3eb8f9ce5bad0d08061b9cc4c901461824b
Caio Leitão <kylespace@icloud.com>
2023-02-10 08:50:27 -0300

    Update ContaBanco.java

--- src/main/java/com/mycompany/minibanco/ContaBanco.java
+++ src/main/java/com/mycompany/minibanco/ContaBanco.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.minibanco;
+package minibanco;
 
 /**
  *
@@ -15,75 +15,119 @@ public class ContaBanco {
     private float saldo;
     private boolean status;
     
-    public void abrirConta(int nc, String st, String sd, float ss) {
-        
+    public void estadoAtual() {
+        System.out.println("Conta: " + this.getNumConta());
+        System.out.println("Tipo: " + this.getTipo());
+        System.out.println("Dono: " + this.getDono());
+        System.out.println("Saldo: " + this.getSaldo());
+        System.out.println("Status: " + this.getStatus());
+    }
+    
+    public void abrirConta(String t) {
+        this.setTipo(t);
+        this.setStatus(true);
+        if ("CC".equals(t)) {
+            this.setSaldo(50);
+        } else if ("CP".equals(t)) {
+            this.setSaldo(150);
+        } else {
+            System.out.println("Opção de conta invalida");
+        }
+        System.out.println("Conta aberta com sucesso");
     }
     
     public void fecharConta() {
-        
-    }
-    
-    public void depositar() {
-        
-    }
-    
-    public void sacar() {
-        
-    }
-    
-    public void pagarMensal(){
-        
-    }
-    
-    public int getnumConta() {
-        return this.numConta;
-    }
-    
-    public void setnumConta(int nc){
-        this.numConta = nc;
-    }
-    
-    public String tipo(){
-        return this.tipo;
-    }
-    
-    public void setTipo(String st) {
-        this.tipo = st;
-    }
-    
-    public String getDono(){
-        return this.dono;
-    }
-    
-    public void setDono(String gd) {
-        this.dono = gd;
-    }
-    
-    public float getSaldo() {
-        return this.saldo;
-    }
-    
-    public void setSaldo(float ss) {
-        this.saldo = ss;
-    }
-    
-    public boolean getStatus() {
-        return this.status;
-    }
-    
-    public void setStatus(boolean sss) {
-        this.status = sss;
-    }
-    
-    public void infoConta() {
-        System.out.println("Informações refente a conta " + this.numConta);
-        System.out.println("Tipo: " + this.tipo);
-        System.out.println("Dono: " + this.dono);
-        System.out.println("Saldo: " + this.saldo);
-        if (status == true) {
-            System.out.println("Status da conta: Ativada");
+        if (this.getSaldo() > 0 ) {
+            System.out.println("Conta com dinheiro");
+        } else if (this.getSaldo() < 0) {
+            System.out.println("Conta em débito");
         } else {
-            System.out.println("Status da conta: Desativada");
+            System.out.println("Conta fechada com sucesso");
+            this.setStatus(false);
         }
     }
+    
+    public void depositar(float v) {
+        if (this.getStatus() == true) {
+            this.setSaldo(getSaldo() + v);
+            System.out.println("Deposito de R$" + v + "realizado com sucesso na conta " + this.getDono());
+        } else {
+            System.out.println("A conta se encontra fechada, impossivel depositar");
+        }
+    }
+    public void sacar(float v) {
+        if (this.getStatus() == true) {
+            if (this.getSaldo() >= v) {
+                this.setSaldo(this.getSaldo() - v);
+                System.out.println("Saldo realizado com sucesso, saldo final ficou em R$" + getSaldo());
+            } else {
+                System.out.println("Saldo insuficiente");
+            }
+        } else {
+            System.out.println("Impossivel sacar");
+        }
+    }
+    public void pagarMensal() {
+        int v = 0;
+        if ("CC".equals(this.getTipo())) {
+            v = 12;
+        } else if ("CP".equals(this.getTipo())) {
+            v = 20;
+        }
+        if (this.getStatus() == true) {
+            if (this.getSaldo() > v) {
+                this.setSaldo(getSaldo() - v);
+            } else {
+                System.out.println("Saldo insuficiente");
+            }
+        } else {
+            System.out.println("Impossivel pagar");
+        }
+    }
+    public ContaBanco() {
+        this.setSaldo(0);
+        this.setStatus(false);
+    }
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
+    
 }
